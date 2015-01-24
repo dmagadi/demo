9d34d567b9efb1988a92e7b68c7eb582519a1343
Aamir Godil <godil.aamir1@gmail.com>
2015-01-24 12:10:28 -0800

    added main method

---
+++ Contact Management System/src/com/google/godil/aamir1/ContactDemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.google.godil.aamir1;
+
+/**
+ *
+ * @author Aamir
+ */
+public class ContactDemo {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
