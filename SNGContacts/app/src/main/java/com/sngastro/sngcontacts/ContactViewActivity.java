4eb6727e1ee1b34b9b5544717c2f7af5a57cb19c
Aamir Godil <godil.aamir1@gmail.com>
2015-07-19 23:16:39 -0700

    call confirmation

--- SNGContacts/app/src/main/java/com/sngastro/sngcontacts/ContactViewActivity.java
+++ SNGContacts/app/src/main/java/com/sngastro/sngcontacts/ContactViewActivity.java
@@ -1,15 +1,15 @@
 package com.sngastro.sngcontacts;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -34,99 +34,60 @@ public class ContactViewActivity extends ActionBarActivity {
         cellCallButton.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + contactInfo.getCell()));
-                startActivity(i);
-                //finish();
+                new AlertDialog.Builder(ContactViewActivity.this).setMessage("Call Number").
+                        setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("Call", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:1" + contactInfo.getCell()));
+                                startActivity(i);
+                            }
+                        }).show();
+                Button homeCallButton = (Button) findViewById(R.id.homeCallButton);
+                homeCallButton.setOnClickListener(new Button.OnClickListener() {
+
+                    public void onClick(View v) {
+                        new AlertDialog.Builder(ContactViewActivity.this).setMessage("Call Number").
+                                setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int which) {
+
+                                    }
+                                }).
+                                setPositiveButton("Call", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:1" + contactInfo.getHome()));
+                                        startActivity(i);
+                                    }
+                                }).show();
+                    }
+                });
             }
-
         });
-        Button homeCallButton = (Button) findViewById(R.id.homeCallButton);
-        homeCallButton.setOnClickListener(new Button.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + contactInfo.getHome()));
-                startActivity(i);
-            }
-
-        });
-
-
-        Log.i(TAG, "onCreate");
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_home:
-                finish();
-                return true;
-
-            default:
-                return true;
-        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_contact_view, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart");
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume");
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause");
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop");
-    }
-
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(TAG, "onRestart");
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy");
-    }
-
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.i(TAG, "onSaveInstanceState");
-    }
-
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.i(TAG, "onRestoreInstanceState");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_home:
+                finish();
+                return true;
+            default:
+                return true;
+        }
     }
 
 }
